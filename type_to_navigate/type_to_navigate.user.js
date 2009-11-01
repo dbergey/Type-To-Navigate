23f578fbab483a3c08e0e51d178f5ac231fdadc2
Daniel Bergey <dbergey@gmail.com>
2009-10-31 22:49:27 -0400

    Added keypresses indicator.

--- type_to_navigate/type_to_navigate.user.js
+++ type_to_navigate/type_to_navigate.user.js
@@ -11,12 +11,15 @@
 	var searchString = '';
 	var nextSearchString = '';
 	var keyupTimeout;
+	var indicator;
+	var indicatorInner;
 	
 	function trim(str) { return str.match(/^\s*(.*?)\s*$/)[1]; };
 	
 	function focusedElement() {
 		var el = document.activeElement;
-		return (( el.tagName.match(/INPUT|TEXTAREA/) && el.getAttribute('type').match(/^|text|search|password$/) ) || el.getAttribute('contenteditable') == 'true') ? el : false;
+		var computedStyle = window.getComputedStyle(el);
+		return (( el.tagName.match(/INPUT|TEXTAREA/) && (el.getAttribute('type') || '').match(/^|text|search|password$/) ) || el.getAttribute('contenteditable') == 'true' || computedStyle['-webkit-user-modify'] != 'read-only') ? el : false;
 	};
 
 	function focusSelectedLink(str) {
@@ -40,6 +43,43 @@
 		}
 	};
 	
+	function createIndicator() {
+		// create indicator
+		var container = document.createElement('div');
+		container.innerHTML = '<div id="type_to_select_keys">\
+			<style>\
+			#type_to_select_keys {\
+				position: fixed;\
+				left: 0;\
+				right: 0;\
+				bottom: 10%;\
+				text-align: center;\
+				opacity: 0;\
+				font: 18px arial;\
+				-webkit-transition: opacity .25s linear;\
+			}\
+			#type_to_select_keys_inner {\
+				background: rgba(0, 0, 0, 0.75);\
+				-webkit-border-radius: 8px;\
+				border: 2px solid rgba(255, 255, 255, 0.75);\
+				-webkit-box-shadow: 0 3px 25px rgba(0, 0, 0, 0.75);\
+				margin: 0 auto;\
+				display: inline-block;\
+				padding: 8px;\
+				color: white;\
+			}\
+			</style>\
+			<div id="type_to_select_keys_inner"></div>\
+		</div>';
+		document.body.appendChild(indicator = container.childNodes[0]);
+		indicatorInner = document.getElementById('type_to_select_keys_inner');
+	}
+	
+	if ( document.readyState == 'complete' ) createIndicator();
+		else window.addEventListener('load', function() {
+			createIndicator();
+		});
+		
 	// handle command-g & esc
 	window.addEventListener('keydown', function(e) {
 		e.cmdKey = e.metaKey && !e.ctrlKey;
@@ -48,6 +88,7 @@
 		// handle esc in fields (blur)
 		if ( focusedElement() && e.keyCode == 27 ) {
 			focusedElement().blur();
+			return false;
 		}
 		
 		// if cmd-g and we have go to next
@@ -65,7 +106,6 @@
 	window.addEventListener('keypress', function(e) {
 		e.cmdKey = e.metaKey && !e.ctrlKey;
 		e.character = String.fromCharCode(e.keyCode);
-	
 		// if it was a typeable character, Cmd key wasn't down, and a field doesn't have focus, and char isn't return or space
 		if ( e.keyCode && !focusedElement() && !e.cmdKey && e.keyCode != 13 && e.character != ' ' ) {
 			
@@ -75,9 +115,20 @@
 			
 			// postpone clearing
 			clearTimeout(keyupTimeout);
-			
+			if ( indicator ) {
+				indicatorInner.innerHTML = searchString;
+				indicator.style['-webkit-transition'] = 'none';
+				indicator.style.opacity = 1.0;
+			}
+		
 			keyupTimeout = setTimeout(function() {
 				searchString = '';
+				
+				// indicator
+				if ( indicator ) {
+					indicator.style['-webkit-transition'] = null;
+					indicator.style.opacity = 0.0;
+				}
 			}, 1000);
 			
 			// clear selection and find again
